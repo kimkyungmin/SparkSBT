e85595a8f75ee903836be3ee4bce430926c18801
김경민 <kimkyungmin@tmon.co.kr>
2016-03-22 18:41:27 +0900

    json 처리시 mapPartition/flatMap쓴 이유 확인

--- src/main/scala/com/tmon/study/ch5/JsonReadSimple1.scala
+++ src/main/scala/com/tmon/study/ch5/JsonReadSimple1.scala
@@ -5,38 +5,25 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import org.apache.spark.{SparkConf, SparkContext}
 
-/**
-  * Created by kimkyungmin on 2016-03-22.
-  */
 
-case class Person(name: String, lovesPandas: Boolean)
-
-object JsonRead {
+object JsonReadSimple1 {
   def main(args: Array[String]) {
     val conf = new SparkConf().setMaster("local").setAppName("BasicJson")
     val sc = new SparkContext(conf)
 
     val inputFile = "files/pandainfo.json"
     val outputFile = "files/result"
-    val input = sc.textFile(inputFile)
+    val input = sc.textFile(inputFile, 10)
+    println("input's partition size :: " + input.partitions.size)
 
-    //mapPartitions를 쓴 이유는 ObjectMapper가 serializable되지 않음으로, singleton으로 각 드라이버에 보낼 수 없다.
-    //그래서 각 파티션별로 objectMapper를 생성하도록 했다. (비싼 연산이긴하다)
-    val result = input.mapPartitions(records => {
+    val result = input.map(record => {
       val mapper = new ObjectMapper with ScalaObjectMapper
       mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
       mapper.registerModule(DefaultScalaModule)
-
-      //flatMap을 쓴 이유는 exception발생시 해당 내용을 무시(empty list, None)하기 위해서다.
-      //정상적인 data는 Some(_)으로 쌓였다가 flat을 하면서 다시 벗겨지게 된다.
-      records.flatMap(record => {
-        try {
-          Some(mapper.readValue(record, classOf[Person]))
-        } catch {
-          case e: Exception => None
-        }
-      })
-    }, true)
+      mapper.readValue[Person](record, classOf[Person])
+    })
+    println("Read Json===== ")
+    println(result.collect().mkString("\n"))
 
     val outRDD = result.filter(_.lovesPandas).mapPartitions(records => {
       val mapper = new ObjectMapper with ScalaObjectMapper
@@ -47,4 +34,5 @@ object JsonRead {
     println(outRDD.collect().mkString("\n"))
 
   }
+
 }
